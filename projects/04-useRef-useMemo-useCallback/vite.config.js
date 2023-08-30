97f76990416aa4bdd2b12622f4ca9c1f2c9148e4
Kevin Moreno <kevinantonioc13@gmail.com>
2023-08-29 23:21:13 -0600

    deploy config

--- projects/04-useRef-useMemo-useCallback/vite.config.js
+++ projects/04-useRef-useMemo-useCallback/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react/tree/master/projects/04-useRef-useMemo-useCallback/'
+  base: '/Movies_Search/'
 })
