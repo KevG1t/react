2485c891b05764b77a1d5f8460dd2808989cbcd5
Kevin Moreno <kevinantonioc13@gmail.com>
2023-10-15 03:00:19 -0600

    add submodules

--- projects/08-zustand-JavaScript-Quizz/src/services/questions.js
+++
@@ -1,7 +0,0 @@
-const API_URL = import.meta.env.PROD ? 'https://kevg1t.github.io/JavaScriptQuizz/' : 'http://localhost:5173/JavaScriptQuizz/'
-
-export async function getQuestions () {
-  const response = await fetch(`${API_URL}/questions.json`)
-  const data = await response.json()
-  return data
-}
